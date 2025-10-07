219987adb46601a264c1abba85e83b6a2bc6a760
Lincoln Jones <lincolnjones226@gmail.com>
2025-10-07 07:13:18 -0400

    title screen transition update

--- Into the Abyss/Scripts/title_screen.gd
+++ Into the Abyss/Scripts/title_screen.gd
@@ -9,5 +9,5 @@ func _process(delta: float) -> void:
 	if Input.is_anything_pressed() and not fading:
 		$AnimationPlayer.play("fade")
 		fading = true
-		await get_tree().create_timer(1).timeout
+		await get_tree().create_timer(1.5).timeout
 		queue_free()
