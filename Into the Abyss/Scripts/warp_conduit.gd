71504a7fc26b319fe69f2b5d37ff29012808174e
Lincoln Jones <lincolnjones226@gmail.com>
2026-08-01 21:23:29 -0400

    made the warp conduit work

--- Into the Abyss/Scripts/warp_conduit.gd
+++ Into the Abyss/Scripts/warp_conduit.gd
@@ -23,6 +23,6 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		player_on = true
+		player_on = false
 		var tween = get_tree().create_tween()
 		tween.tween_property($"Label", "modulate", Color.TRANSPARENT, 0.25)
